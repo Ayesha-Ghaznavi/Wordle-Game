031c42673d2a1a9be8b04af2ba4377f322d6db94
Ayesha-Ghaznavi <152681871+Ayesha-Ghaznavi@users.noreply.github.com>
2023-12-25 16:56:48 +0500

    Update wordle.cpp

--- wordle.cpp
+++ wordle.cpp
@@ -7,8 +7,8 @@
 #include "SFML/Graphics.hpp"
 #include "SFML/Window/Mouse.hpp"
 //including classes
-#include "Button.h"
-#include "TextObject.h"
+#include "consolebuttons.h"
+#include "textandcolours.h"
 
 using namespace std;
 
@@ -29,80 +29,80 @@ sf::Font font;
 int fontSize = 30;
 
 //defining 2D vectors of TextObject
-vector<vector<TextObject>> textObjects = { {
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(260, 110), objectColour, sf::Text("Q", font, fontSize)), // [0][0] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(320, 110), objectColour, sf::Text("Q", font, fontSize)), // [0][1] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(380, 110), objectColour, sf::Text("Q", font, fontSize)), // [0][2] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(440, 110), objectColour, sf::Text("Q", font, fontSize)), // [0][3] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(500, 110), objectColour, sf::Text("Q", font, fontSize)) // [0][4] 
+vector<vector<textandcolours>> textObjects = { {
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(260, 110), objectColour, sf::Text("Q", font, fontSize)), // [0][0] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(320, 110), objectColour, sf::Text("Q", font, fontSize)), // [0][1] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(380, 110), objectColour, sf::Text("Q", font, fontSize)), // [0][2] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(440, 110), objectColour, sf::Text("Q", font, fontSize)), // [0][3] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(500, 110), objectColour, sf::Text("Q", font, fontSize)) // [0][4] 
         }, {
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(260, 110 + 60), objectColour, sf::Text("Q", font, fontSize)), // [1][0] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(320, 110 + 60), objectColour, sf::Text("Q", font, fontSize)), // [1][1] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(380, 110 + 60), objectColour, sf::Text("Q", font, fontSize)), // [1][2] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(440, 110 + 60), objectColour, sf::Text("Q", font, fontSize)), // [1][3] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(500, 110 + 60), objectColour, sf::Text("Q", font, fontSize)) // [1][4] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(260, 110 + 60), objectColour, sf::Text("Q", font, fontSize)), // [1][0] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(320, 110 + 60), objectColour, sf::Text("Q", font, fontSize)), // [1][1] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(380, 110 + 60), objectColour, sf::Text("Q", font, fontSize)), // [1][2] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(440, 110 + 60), objectColour, sf::Text("Q", font, fontSize)), // [1][3] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(500, 110 + 60), objectColour, sf::Text("Q", font, fontSize)) // [1][4] 
         },{
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(260, 110 + 120), objectColour, sf::Text("Q", font, fontSize)), // [2][0] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(320, 110 + 120), objectColour, sf::Text("Q", font, fontSize)), // [2][1] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(380, 110 + 120), objectColour, sf::Text("Q", font, fontSize)), // [2][2] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(440, 110 + 120), objectColour, sf::Text("Q", font, fontSize)), // [2][3] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(500, 110 + 120), objectColour, sf::Text("Q", font, fontSize)) // [2][4] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(260, 110 + 120), objectColour, sf::Text("Q", font, fontSize)), // [2][0] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(320, 110 + 120), objectColour, sf::Text("Q", font, fontSize)), // [2][1] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(380, 110 + 120), objectColour, sf::Text("Q", font, fontSize)), // [2][2] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(440, 110 + 120), objectColour, sf::Text("Q", font, fontSize)), // [2][3] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(500, 110 + 120), objectColour, sf::Text("Q", font, fontSize)) // [2][4] 
         },{
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(260, 110 + 180), objectColour, sf::Text("Q", font, fontSize)), // [3][0] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(320, 110 + 180), objectColour, sf::Text("Q", font, fontSize)), // [3][1] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(380, 110 + 180), objectColour, sf::Text("Q", font, fontSize)), // [3][2] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(440, 110 + 180), objectColour, sf::Text("Q", font, fontSize)), // [3][3] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(500, 110 + 180), objectColour, sf::Text("Q", font, fontSize)) // [3][4] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(260, 110 + 180), objectColour, sf::Text("Q", font, fontSize)), // [3][0] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(320, 110 + 180), objectColour, sf::Text("Q", font, fontSize)), // [3][1] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(380, 110 + 180), objectColour, sf::Text("Q", font, fontSize)), // [3][2] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(440, 110 + 180), objectColour, sf::Text("Q", font, fontSize)), // [3][3] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(500, 110 + 180), objectColour, sf::Text("Q", font, fontSize)) // [3][4] 
         },{
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(260, 110 + 240), objectColour, sf::Text("Q", font, fontSize)), // [4][0] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(320, 110 + 240), objectColour, sf::Text("Q", font, fontSize)), // [4][1] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(380, 110 + 240), objectColour, sf::Text("Q", font, fontSize)), // [4][2] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(440, 110 + 240), objectColour, sf::Text("Q", font, fontSize)), // [4][3] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(500, 110 + 240), objectColour, sf::Text("Q", font, fontSize)) // [4][4] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(260, 110 + 240), objectColour, sf::Text("Q", font, fontSize)), // [4][0] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(320, 110 + 240), objectColour, sf::Text("Q", font, fontSize)), // [4][1] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(380, 110 + 240), objectColour, sf::Text("Q", font, fontSize)), // [4][2] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(440, 110 + 240), objectColour, sf::Text("Q", font, fontSize)), // [4][3] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(500, 110 + 240), objectColour, sf::Text("Q", font, fontSize)) // [4][4] 
         },{
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(260, 110 + 300), objectColour, sf::Text("Q", font, fontSize)), // [5][0] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(320, 110 + 300), objectColour, sf::Text("Q", font, fontSize)), // [5][1] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(380, 110 + 300), objectColour, sf::Text("Q", font, fontSize)), // [5][2] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(440, 110 + 300), objectColour, sf::Text("Q", font, fontSize)), // [5][3] 
-        TextObject(sf::Vector2f(50, 50), sf::Vector2f(500, 110 + 300), objectColour, sf::Text("Q", font, fontSize)) // [5][4] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(260, 110 + 300), objectColour, sf::Text("Q", font, fontSize)), // [5][0] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(320, 110 + 300), objectColour, sf::Text("Q", font, fontSize)), // [5][1] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(380, 110 + 300), objectColour, sf::Text("Q", font, fontSize)), // [5][2] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(440, 110 + 300), objectColour, sf::Text("Q", font, fontSize)), // [5][3] 
+        textandcolours(sf::Vector2f(50, 50), sf::Vector2f(500, 110 + 300), objectColour, sf::Text("Q", font, fontSize)) // [5][4] 
         }
 };
 
 //defining vectors of Buttons
-vector<Button> buttons = {
-    Button(sf::Vector2f(50, 50), sf::Vector2f(140, 640), buttonColour, sf::Text("A", font, fontSize), Button::ButtonType::Letter), // a
-    Button(sf::Vector2f(50, 50), sf::Vector2f(440, 700), buttonColour, sf::Text("B", font, fontSize), Button::ButtonType::Letter), // b
-    Button(sf::Vector2f(50, 50), sf::Vector2f(320, 700), buttonColour, sf::Text("C", font, fontSize), Button::ButtonType::Letter), // c
-    Button(sf::Vector2f(50, 50), sf::Vector2f(260, 640), buttonColour, sf::Text("D", font, fontSize), Button::ButtonType::Letter), // d
-    Button(sf::Vector2f(50, 50), sf::Vector2f(230, 580), buttonColour, sf::Text("E", font, fontSize), Button::ButtonType::Letter), // e
-    Button(sf::Vector2f(50, 50), sf::Vector2f(320, 640), buttonColour, sf::Text("F", font, fontSize), Button::ButtonType::Letter), // f
-    Button(sf::Vector2f(50, 50), sf::Vector2f(380, 640), buttonColour, sf::Text("G", font, fontSize), Button::ButtonType::Letter), // g
-    Button(sf::Vector2f(50, 50), sf::Vector2f(440, 640), buttonColour, sf::Text("H", font, fontSize), Button::ButtonType::Letter), // h
-    Button(sf::Vector2f(50, 50), sf::Vector2f(530, 580), buttonColour, sf::Text("I", font, fontSize), Button::ButtonType::Letter), // i
-    Button(sf::Vector2f(50, 50), sf::Vector2f(500, 640), buttonColour, sf::Text("J", font, fontSize), Button::ButtonType::Letter), // j
-    Button(sf::Vector2f(50, 50), sf::Vector2f(560, 640), buttonColour, sf::Text("K", font, fontSize), Button::ButtonType::Letter), // k
-    Button(sf::Vector2f(50, 50), sf::Vector2f(629, 640), buttonColour, sf::Text("L", font, fontSize), Button::ButtonType::Letter), // l
-    Button(sf::Vector2f(50, 50), sf::Vector2f(560, 700), buttonColour, sf::Text("M", font, fontSize), Button::ButtonType::Letter), // m
-    Button(sf::Vector2f(50, 50), sf::Vector2f(500, 700), buttonColour, sf::Text("N", font, fontSize), Button::ButtonType::Letter), // n
-    Button(sf::Vector2f(50, 50), sf::Vector2f(590, 580), buttonColour, sf::Text("O", font, fontSize), Button::ButtonType::Letter), // o
-    Button(sf::Vector2f(50, 50), sf::Vector2f(650, 580), buttonColour, sf::Text("P", font, fontSize), Button::ButtonType::Letter), // p
-    Button(sf::Vector2f(50, 50), sf::Vector2f(110, 580), buttonColour, sf::Text("Q", font, fontSize), Button::ButtonType::Letter), // q
-    Button(sf::Vector2f(50, 50), sf::Vector2f(290, 580), buttonColour, sf::Text("R", font, fontSize), Button::ButtonType::Letter), // r
-    Button(sf::Vector2f(50, 50), sf::Vector2f(200, 640), buttonColour, sf::Text("S", font, fontSize), Button::ButtonType::Letter), // s
-    Button(sf::Vector2f(50, 50), sf::Vector2f(350, 580), buttonColour, sf::Text("T", font, fontSize), Button::ButtonType::Letter), // t
-    Button(sf::Vector2f(50, 50), sf::Vector2f(470, 580), buttonColour, sf::Text("U", font, fontSize), Button::ButtonType::Letter), // u
-    Button(sf::Vector2f(50, 50), sf::Vector2f(380, 700), buttonColour, sf::Text("V", font, fontSize), Button::ButtonType::Letter), // v
-    Button(sf::Vector2f(50, 50), sf::Vector2f(170, 580), buttonColour, sf::Text("W", font, fontSize), Button::ButtonType::Letter), // w
-    Button(sf::Vector2f(50, 50), sf::Vector2f(260, 700), buttonColour, sf::Text("X", font, fontSize), Button::ButtonType::Letter), // x
-    Button(sf::Vector2f(50, 50), sf::Vector2f(410, 580), buttonColour, sf::Text("Y", font, fontSize), Button::ButtonType::Letter), // y
-    Button(sf::Vector2f(50, 50), sf::Vector2f(200, 700), buttonColour, sf::Text("Z", font, fontSize), Button::ButtonType::Letter), // z
+vector<consolebuttons> buttons = {
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(140, 640), buttonColour, sf::Text("A", font, fontSize), consolebuttons::ButtonType::Letter), // a
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(440, 700), buttonColour, sf::Text("B", font, fontSize), consolebuttons::ButtonType::Letter), // b
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(320, 700), buttonColour, sf::Text("C", font, fontSize), consolebuttons::ButtonType::Letter), // c
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(260, 640), buttonColour, sf::Text("D", font, fontSize), consolebuttons::ButtonType::Letter), // d
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(230, 580), buttonColour, sf::Text("E", font, fontSize), consolebuttons::ButtonType::Letter), // e
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(320, 640), buttonColour, sf::Text("F", font, fontSize), consolebuttons::ButtonType::Letter), // f
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(380, 640), buttonColour, sf::Text("G", font, fontSize), consolebuttons::ButtonType::Letter), // g
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(440, 640), buttonColour, sf::Text("H", font, fontSize), consolebuttons::ButtonType::Letter), // h
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(530, 580), buttonColour, sf::Text("I", font, fontSize), consolebuttons::ButtonType::Letter), // i
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(500, 640), buttonColour, sf::Text("J", font, fontSize), consolebuttons::ButtonType::Letter), // j
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(560, 640), buttonColour, sf::Text("K", font, fontSize), consolebuttons::ButtonType::Letter), // k
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(629, 640), buttonColour, sf::Text("L", font, fontSize), consolebuttons::ButtonType::Letter), // l
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(560, 700), buttonColour, sf::Text("M", font, fontSize), consolebuttons::ButtonType::Letter), // m
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(500, 700), buttonColour, sf::Text("N", font, fontSize), consolebuttons::ButtonType::Letter), // n
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(590, 580), buttonColour, sf::Text("O", font, fontSize), consolebuttons::ButtonType::Letter), // o
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(650, 580), buttonColour, sf::Text("P", font, fontSize), consolebuttons::ButtonType::Letter), // p
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(110, 580), buttonColour, sf::Text("Q", font, fontSize), consolebuttons::ButtonType::Letter), // q
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(290, 580), buttonColour, sf::Text("R", font, fontSize), consolebuttons::ButtonType::Letter), // r
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(200, 640), buttonColour, sf::Text("S", font, fontSize), consolebuttons::ButtonType::Letter), // s
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(350, 580), buttonColour, sf::Text("T", font, fontSize), consolebuttons::ButtonType::Letter), // t
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(470, 580), buttonColour, sf::Text("U", font, fontSize), consolebuttons::ButtonType::Letter), // u
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(380, 700), buttonColour, sf::Text("V", font, fontSize), consolebuttons::ButtonType::Letter), // v
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(170, 580), buttonColour, sf::Text("W", font, fontSize), consolebuttons::ButtonType::Letter), // w
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(260, 700), buttonColour, sf::Text("X", font, fontSize), consolebuttons::ButtonType::Letter), // x
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(410, 580), buttonColour, sf::Text("Y", font, fontSize), consolebuttons::ButtonType::Letter), // y
+    consolebuttons(sf::Vector2f(50, 50), sf::Vector2f(200, 700), buttonColour, sf::Text("Z", font, fontSize), consolebuttons::ButtonType::Letter), // z
 
-    Button(sf::Vector2f(100, 50), sf::Vector2f(50, 700), buttonColour, sf::Text("Enter", font, fontSize), Button::ButtonType::Enter), // enter
+    consolebuttons(sf::Vector2f(100, 50), sf::Vector2f(50, 700), buttonColour, sf::Text("Enter", font, fontSize), consolebuttons::ButtonType::Enter), // enter
 
-    Button(sf::Vector2f(100, 50), sf::Vector2f(650, 700), buttonColour, sf::Text("Delete", font, fontSize), Button::ButtonType::Delete) // backspace
+    consolebuttons(sf::Vector2f(100, 50), sf::Vector2f(650, 700), buttonColour, sf::Text("Delete", font, fontSize), consolebuttons::ButtonType::Delete) // backspace
 };
 sf::Text nText = sf::Text("igozdev", font, 20);
-Button resetButton(sf::Vector2f(100, 50), sf::Vector2f(360, 30), buttonColour, sf::Text("Reset", font, fontSize), Button::ButtonType::Reset);
+consolebuttons resetButton(sf::Vector2f(100, 50), sf::Vector2f(360, 30), buttonColour, sf::Text("Reset", font, fontSize), consolebuttons::ButtonType::Reset);
 
 sf::Text endText = sf::Text("", font, 25);
 
@@ -306,29 +306,29 @@ void Reset()
     GenWord();
 }
 
-void ButtonAction(Button button)
+void ButtonAction(consolebuttons button)
 {
-    Button::ButtonType type = button.GetButtonType();
+    consolebuttons::ButtonType type = button.GetButtonType();
     switch (type)
     {
-    case Button::ButtonType::Letter:
+    case consolebuttons::ButtonType::Letter:
         cout << "\nLetter: " << (char)button.GetText()[0];
         AddLetter((char)button.GetText()[0]);
         UpdateTextObjectText();
         break;
 
-    case Button::ButtonType::Enter:
+    case consolebuttons::ButtonType::Enter:
         cout << "\nEnter";
         Enter();
         break;
 
-    case Button::ButtonType::Delete:
+    case consolebuttons::ButtonType::Delete:
         cout << "\nDelete";
         RemoveLetter();
         UpdateTextObjectText();
         break;
 
-    case Button::ButtonType::Reset:
+    case consolebuttons::ButtonType::Reset:
         cout << "\nReset";
         Reset();
         break;
@@ -368,7 +368,7 @@ int main()
             if (event.type == sf::Event::MouseButtonPressed)
             {
                 if (keyboardButtonsEnabled)
-                    for (Button button : buttons)
+                    for (consolebuttons button : buttons)
                         if (button.MouseCollides(mouse, window))
                             ButtonAction(button);
                 if (resetButton.MouseCollides(mouse, window))
@@ -377,10 +377,10 @@ int main()
         }
 
         window.clear(sf::Color(20, 30, 50)); // Color background
-        for (Button button : buttons)
+        for (consolebuttons button : buttons)
             button.DrawObject(window);
-        for (vector<TextObject> textObjectVec : textObjects)
-            for (TextObject textObject : textObjectVec)
+        for (vector<textandcolours> textObjectVec : textObjects)
+            for (textandcolours textObject : textObjectVec)
                 textObject.DrawObject(window);
         resetButton.DrawObject(window);
 
